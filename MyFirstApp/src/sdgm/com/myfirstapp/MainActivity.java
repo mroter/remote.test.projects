e26382401b86223d6a4f2169da1456a7f6d82d68
M.Roter <M.Roter@lap-matty8.sd-develop.local>
2013-12-05 17:09:36 +0200

    Initial commit

---
+++ MyFirstApp/src/sdgm/com/myfirstapp/MainActivity.java
@@ -0,0 +1,78 @@
+package sdgm.com.myfirstapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+	public final static String EXTRA_MESSAGE = "sdgm.com.myfirstapp.MESSAGE";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		
+		return super.onCreateOptionsMenu(menu);
+		//return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_search:
+	            openSearch();
+	            return true;
+	        case R.id.action_settings:
+	            openSettings();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	
+	private void openSettings() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void openSearch() {
+		// Retrieve  the text from the EditText widget 
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		
+		// Store the test in the activity preference file
+		SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+		SharedPreferences.Editor editor = sharedPref.edit();
+		editor.putString(EXTRA_MESSAGE, message);
+		editor.commit();
+	}
+
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+	    // Do something in response to button
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		//EditText editText = (EditText) findViewById(R.id.edit_message);
+		//String message = editText.getText().toString();
+		
+		// Retrieve  the text from the activity preference file
+		SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+		String message = sharedPref.getString(EXTRA_MESSAGE, "Not found.");
+		
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
+
+}
